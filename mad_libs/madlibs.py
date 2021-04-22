28a544c159a241d663a684e2869ad7a3632c8201
kaci65 <wanjikukarugi@gmail.com>
2021-04-22 17:11:13 +0300

    Fix pep8

--- mad_libs/madlibs.py
+++ mad_libs/madlibs.py
@@ -13,14 +13,14 @@ story = f"{exclamation}! he said {word_adv} as he jumped into his convertible {w
 
 print(story)
 
-# # using str.format() instead of 'f-string'
-# # stoy variables
+# using str.format() instead of 'f-string'
+# stoy variables
 # exclamation = input("Enter an exclamation word and hit enter: ")
 # word_adv = input("Enter an adverb and hit enter: ")
 # word_noun = input("Enter a noun and hit enter: ")
 # word_adj = input("Enter an adjective and hit enter: ")
 
-# # story
+# story
 # story = "{}! he said {} as he jumped into his convertible {} and drove off with his {} wife."
 
 # print(story.format(exclamation, word_adv, word_noun, word_adj))
